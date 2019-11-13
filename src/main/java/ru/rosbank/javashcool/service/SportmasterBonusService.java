763012a2270a9041b097a4ed7cfdaccbee6f0245
POMSPOS14 <POMAH14021997@MAIL.RU>
2019-11-13 17:48:23 +0300

    fixes #2

--- src/main/java/ru/rosbank/javashcool/service/SportmasterBonusService.java
+++ src/main/java/ru/rosbank/javashcool/service/SportmasterBonusService.java
@@ -2,16 +2,28 @@ package ru.rosbank.javashcool.service;
 
 public class SportmasterBonusService {
     public int calculator(int spentSum, int accumulatedSum) {
-        int bonusRatio = 50;
+        int blueRange = 15_000;
+        int silverRange =150_000;
+        int bonusRatio;
 
-        if (accumulatedSum > 15_000 && accumulatedSum <= 150_000) {
-            bonusRatio = 70;
-        } else if (accumulatedSum > 150_000) {
+
+
+        if(accumulatedSum > silverRange){
             bonusRatio = 100;
+            return resultBonus( spentSum, bonusRatio);
         }
 
-        int resultBonus = spentSum * bonusRatio / 1000;
+        if(accumulatedSum > blueRange){
+            bonusRatio = 70;
+            return resultBonus( spentSum, bonusRatio);
+        }
 
-        return resultBonus;
+        bonusRatio = 50;
+        return resultBonus( spentSum, bonusRatio);
+    }
+
+    int resultBonus(int spentSum, int bonusRatio){
+
+        return spentSum * bonusRatio / 1000;
     }
 }
